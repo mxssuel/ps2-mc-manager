c4085afcc667e4f90b3415268978f473a8ec6b87
mxssuel <devmaxssuel@gmail.com>
2025-03-14 13:50:16 -0300

    Documentando e fazendo alguns pequenos ajustes no módulo de bytes!!!

--- src/js/utils/bytes.js
+++ src/js/utils/bytes.js
@@ -1,11 +1,28 @@
+/**
+ * Lê um valor de 16 bits (uint16) a partir de um array de bytes.
+ * 
+ * @param {Uint8Array} bytes Array de bytes.
+ * @param {number} index Índice inicial.
+ * @returns {number} Valor de 16 bits.
+ */
 export function readUint16(bytes, index) {
-    return bytes[index] | (bytes[index + 1] << 8);
+    return bytes[index] | (bytes[index + 1] << 8)
 }
 
-export function readUint32(bytes, [start, end]) {
-    const view = new Uint8Array(
-        bytes.buffer, start, end - start + 1 // n+1
-    )
+/**
+ * Lê um valor de 32 bits (uint32) a partir de um array de bytes.
+ * 
+ * @param {Uint8Array} bytes Array de bytes.
+ * @param {number} index Índice inicial.
+ * @returns {number} Valor de 32 bits.
+ */
+export function readUint32(bytes, index) {
+    //return (view[0] << 0) | (view[1] << 8) | (view[2] << 16) | (view[3] << 24)
 
-    return (view[0] << 0) | (view[1] << 8) | (view[2] << 16) | (view[3] << 24)
+    /**
+     * Aprendi muito com a solução acima, entretanto, assim
+     * fica mais simples de manter.
+     */
+    const view = new DataView(bytes.buffer, index, 4)
+    return view.getUint32(0, true); // true = little-endian
 }
